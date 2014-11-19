15b30593441561dba313d511ffbe6973c8069cc6
Felix Borzik <borzik1992@gmail.com>
2014-11-19 12:57:01 +0200

    cleanup

--- spec/features/events_edit_spec.rb
+++ spec/features/events_edit_spec.rb
@@ -5,18 +5,10 @@ feature 'Events edit path', js: true do
     @event = Event.create(title: 'event1')
   end
   it 'should create event', js: true do
-    # visit(new_event_path)
-    # fill_in 'Title', with: 'event1'
-    # click_button 'Create'
-    # expect(page.body).to have_text 'event1'
-    # click_link 'Edit'
     visit event_path(@event)
-    # expect(page.body).to have_text 'Editing event'
-    # fill_in 'Title', with: 'Lorem ipsum'
-    # click_button 'Update'
     expect(page.body).to have_text('event1')
     sleep 2
-    visit page.driver.browser.current_url
+    visit events_path
     expect(page.body).to have_text('Changed by AJAX')
   end
 end
